f2c8e26c03a1de1b6b63c0d577c5d428d17e6994
Florian <91699102+Basso42@users.noreply.github.com>
2025-07-22 01:25:05 +0100

    Update vscode-setup.sh

--- vscode-setup.sh
+++ vscode-setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Download files
-# Copilot
+### Copilot
 wget --retry-on-http-error=429 https://marketplace.visualstudio.com/_apis/public/gallery/publishers/GitHub/vsextensions/copilot/1.270.0/vspackage -O copilot.vsix.gz
 wget --retry-on-http-error=429 https://marketplace.visualstudio.com/_apis/public/gallery/publishers/GitHub/vsextensions/copilot-chat/0.23.0/vspackage -O copilot-chat.vsix.gz
 
@@ -13,3 +13,17 @@ rm copilot.vsix
 gunzip copilot-chat.vsix.gz 
 code-server --install-extension copilot-chat.vsix
 rm copilot-chat.vsix
+
+### Shortcuts
+echo '[
+    {
+        "key": "cmd+shift+c",
+        "command": "workbench.action.createTerminalEditor",
+        "when": "terminalProcessSupported"
+    },
+    {
+        "key": "ctrl+d",
+        "command": "editor.action.deleteLines",  # Delete a line
+        "when": "editorTextFocus"
+    }
+]' > "$HOME/.local/share/code-server/User/keybindings.json"
